f6ad7f579b22a9462d56ddbd3b7f81f8bee2c5ab
ActualSailorCat <52355344+ActualSailorCat@users.noreply.github.com>
2021-12-08 02:46:13 -0500

    Update skeleton_saves.js

--- skeleton_saves.js
+++ skeleton_saves.js
@@ -33,10 +33,6 @@ function valkskellysaves(skellymod, abilityName) {
             if (confirmed) {
                 rollArmy = rollArmy + '<b>Hrrrngh! Skeleton Saves!</b><br><br>';
                 let rolltimes = parseInt(html.find('[name=rollnumber]')[0].value);
-                /*for (let y = 0; y < rolltimes; y++) {
-                    rollArmy = (rollArmy + "<div class=\"row\" style=\"padding-bottom:5px\">" + (y + 1) + '. ' + abilityName + ' SAVE [[1d20 + ' + skellymod + ']] | [[1d20 + ' + skellymod + ']]</div>');
-                }*/
-
                 for (let y = 0; y < rolltimes; y++) {
                     let roll1   = new Roll('1d20').roll();    
                     game.dice3d.showForRoll(roll1);
@@ -48,7 +44,6 @@ function valkskellysaves(skellymod, abilityName) {
                     let rollStr1b = ' (' + rollTot1 + ' + ' + skellymod + ')';
                     let rollStr2a = '<b>' + (rollTot2 + skellymod) + '</b>';
                     let rollStr2b = ' (' + rollTot2 + ' + ' + skellymod + ')';
-                    //rollArmy = ( rollArmy + (y + 1) + '. ATK ' + rollStr1a + rollStr1b + '<span style=\"text-align: right !important\"> — DMG [[' + dMG + '+ 2 + ' + valkprof + ']]</span><br>');
                     rollArmy = ( rollArmy + '<div class="row" style="display:flex; border-bottom:1px dotted black; padding: 2px"><div style="flex: 70%; text-align:left; ">' + abilityName + ' Save ' + (y + 1) + '. ' + rollStr1a + rollStr1b + ' ' + rollStr2a + rollStr2b + '</div></div>');
                 }
 
@@ -62,49 +57,6 @@ function valkskellysaves(skellymod, abilityName) {
             }
         }
     }).render(true);
-    // Voidclaw #
-    /*
-    new Dialog({
-        title: "Mass Voidclaw Roller",
-        content: `
-        <form>
-        <div class="form-group">
-        <label>Number of Voidclaws:</label>
-        <input id="rollnumber2" name="rollnumber2"></input>
-        </div>
-        </form>
-        `,
-        buttons: {
-            one: {
-                icon: '<i class="fas fa-check"></i>',
-                label: "Roll!",
-                callback: () => confirmed2 = true
-            },
-            two: {
-                icon: '<i class="fas fa-times"></i>',
-                label: "Cancel",
-                callback: () => confirmed2 = false
-            }
-        },
-        default: "Cancel",
-        close: html => {
-            if (confirmed2) {
-                rollArmy2 = rollArmy2 + '<b>A. Voidclaw saves.</b><br>';
-                let rolltimes = parseInt(html.find('[name=rollnumber2]')[0].value);
-                for (let y = 0; y < rolltimes; y++) {
-                    rollArmy2 = (rollArmy2 + (y + 1) + '. ' + abilityName + ' SAVE [[1d20 + ' + voidmod + ']] | [[1d20 + ' + voidmod + ']]<br>');
-                }
-
-                var chatData2 = {
-                    user: game.user._id,
-                    speaker: ChatMessage.getSpeaker(),
-                    content: rollArmy2
-                };
-
-                ChatMessage.create(chatData2, {});
-            }
-        }
-    }).render(true);*/
 }
 // Function END
 
@@ -131,7 +83,6 @@ new Dialog({
             callback: (html) => {
                 let abilityName = html.find("[name=abilityName]")[0].value;
                 let skelly = game.data.actors.find(c => c.name === "Skeleton Generic");
-                //let voidclaw = game.data.actors.find(c => c.name === "Voidclaw Skeleton 1");
                 switch (abilityName) {
                     case "STR": {
                         let skellymod = skelly.data.abilities.str.mod;
