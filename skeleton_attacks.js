57c695333d6e8664b65de8a2172cfdfff1f8a227
ActualSailorCat <52355344+ActualSailorCat@users.noreply.github.com>
2022-01-10 02:03:29 -0500

    Update skeleton_attacks.js

--- skeleton_attacks.js
+++ skeleton_attacks.js
@@ -1,7 +1,7 @@
 // Function BEGIN
-function sc_massshockingskellies(rollType) {
+async function sc_massshockingskellies(rollType) {
     let confirmed = false;
-    var rollArmy = "<b>Skeletons.</b><br><span style=\"font-size:0.9em\">Aaah! Real skeletons attack with scimitar and bow!</span><br><br>";
+    var rollArmy = "<b>Skeletons.</b><br><span style=\"font-size:0.9em\">Aaah! Real skeletons attack!</span><br><br>";
     new Dialog({
         title: "Mass Skeleton Attack",
         content: `
@@ -33,7 +33,7 @@ function sc_massshockingskellies(rollType) {
                 switch (rollType) {
                     case "NOR":
                         for (let y = 0; y < rolltimes; y++) {
-                            let roll1   = new Roll('1d20').roll();    
+                            let roll1   = await new Roll('1d20').roll();    
                             game.dice3d.showForRoll(roll1);
                             let rollTot1 = roll1.total;
                             let rollStr1a = '<b>' + (rollTot1 + 4) + '</b>';
@@ -48,9 +48,9 @@ function sc_massshockingskellies(rollType) {
                         break;
                     case "ADV":
                          for (let y = 0; y < rolltimes; y++) {
-                            let roll1       = new Roll('1d20').roll();  // Rolls die mechanically.
+                            let roll1       = await new Roll('1d20').roll();  // Rolls die mechanically.
                             game.dice3d.showForRoll(roll1);             // Rolls die visually.
-                            let roll2       = new Roll('1d20').roll();    
+                            let roll2       = await new Roll('1d20').roll();    
                             game.dice3d.showForRoll(roll2);
                             let rollTot1    = roll1.total;
                             let rollTot2    = roll2.total;   
@@ -101,9 +101,9 @@ function sc_massshockingskellies(rollType) {
                         break;
                     case "DIS":
                          for (let y = 0; y < rolltimes; y++) {
-                            let roll1       = new Roll('1d20').roll();  // Rolls die mechanically.
+                            let roll1       = await new Roll('1d20').roll();  // Rolls die mechanically.
                             game.dice3d.showForRoll(roll1);             // Rolls die visually.
-                            let roll2       = new Roll('1d20').roll();    
+                            let roll2       = await new Roll('1d20').roll();    
                             game.dice3d.showForRoll(roll2);
                             let rollTot1    = roll1.total;
                             let rollTot2    = roll2.total;   
